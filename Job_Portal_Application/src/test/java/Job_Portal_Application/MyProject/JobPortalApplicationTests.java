fba28f8d79d4386966a422b07992afc4cdda0163
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-09 12:02:29 +0400

    NEW

---
+++ Job_Portal_Application/src/test/java/Job_Portal_Application/MyProject/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package Job_Portal_Application.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
